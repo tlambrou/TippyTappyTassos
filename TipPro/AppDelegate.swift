1ec83e82073b38317c4fb0f6be13df72aa60a74a
tlambrou <lambrou@gmail.com>
2017-06-09 15:14:20 -0700

    Finished MVP

--- TipPro/AppDelegate.swift
+++ TipPro/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("Hello Make School!")
         return true
     }
 
@@ -41,6 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+  
 
 }
+
 
--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -8,18 +8,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITextFieldDelegate{
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+  @IBOutlet weak var billAmountField: UITextField!
+  
+  @IBOutlet weak var tipSelector: UISegmentedControl!
+  
+  @IBOutlet weak var tipAmountField: UITextField!
+  
+  @IBOutlet weak var totalAmountField: UITextField!
+  
+  @IBAction func calculateTip(_ sender: AnyObject) {
+    calculate()
+  }
+  
+//  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//    calculate()
+//    return true
+//  }
+  
+  func calculate() {
+    guard let billAmount = Double(billAmountField.text!) else {
+      //show error
+      billAmountField.text = ""
+      tipAmountField.text = ""
+      totalAmountField.text = ""
+      return
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var tipPercentage = 0.0
+    
+    switch tipSelector.selectedSegmentIndex {
+    case 0:
+      tipPercentage = 0.15
+    case 1:
+      tipPercentage = 0.18
+    case 2:
+      tipPercentage = 0.20
+    default:
+      break
     }
+    
+    let roundedBillAmount = round(100*billAmount)/100
+    let tipAmount = roundedBillAmount * tipPercentage
+    let roundedTipAmount = round(100*tipAmount)/100
+    let totalAmount = roundedBillAmount + roundedTipAmount
+    
+    if (!billAmountField.isEditing) {
+      billAmountField.text = String(format: "%.2f", roundedBillAmount)
+    }
+    tipAmountField.text = "$" + String(format: "%.2f", roundedTipAmount)
+    totalAmountField.text = "$" + String(format: "%.2f", totalAmount)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
 
-
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    UIApplication.shared.statusBarView?.backgroundColor = #colorLiteral(red: 0.09317999333, green: 0.6117136478, blue: 0.8447505832, alpha: 1)
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    self.billAmountField.endEditing(true)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
 }
+
+class BorderedButton: UIButton {
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    layer.cornerRadius = 5.0
+    
+  }
+}
+
+extension UIApplication {
+  var statusBarView: UIView? {
+    return value(forKey: "statusBar") as? UIView
+  }
+}
+
+extension UISegmentedControl {
+  
+  
+  
+}
+
 
